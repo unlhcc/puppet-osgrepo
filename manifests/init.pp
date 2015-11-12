a4c161b29302315b0bdc549bc6a8601c01818c08
hzhu16 <nautilushj@gmail.com>
2015-11-12 11:10:15 -0600

    updated for osg version 3.3

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,6 +5,54 @@
 #
 
 class osgrepo (
+  $epel_mirrorlist                        = $osgrepo::params::epel_mirrorlist,
+  $epel_baseurl                           = $osgrepo::params::epel_baseurl,
+  $epel_failovermethod                    = $osgrepo::params::epel_failovermethod,
+  $epel_proxy                             = $osgrepo::params::epel_proxy,
+  $epel_enabled                           = $osgrepo::params::epel_enabled,
+  $epel_gpgcheck                          = $osgrepo::params::epel_gpgcheck,
+  $epel_exclude                           = undef,
+  $epel_includepkgs                       = undef,
+  $epel_testing_mirrorlist                = $osgrepo::params::epel_testing_mirrorlist,
+  $epel_testing_baseurl                   = $osgrepo::params::epel_testing_baseurl,
+  $epel_testing_failovermethod            = $osgrepo::params::epel_testing_failovermethod,
+  $epel_testing_proxy                     = $osgrepo::params::epel_testing_proxy,
+  $epel_testing_enabled                   = $osgrepo::params::epel_testing_enabled,
+  $epel_testing_gpgcheck                  = $osgrepo::params::epel_testing_gpgcheck,
+  $epel_testing_exclude                   = undef,
+  $epel_testing_includepkgs               = undef,
+  $epel_source_mirrorlist                 = $osgrepo::params::epel_source_mirrorlist,
+  $epel_source_baseurl                    = $osgrepo::params::epel_source_baseurl,
+  $epel_source_failovermethod             = $osgrepo::params::epel_source_failovermethod,
+  $epel_source_proxy                      = $osgrepo::params::epel_source_proxy,
+  $epel_source_enabled                    = $osgrepo::params::epel_source_enabled,
+  $epel_source_gpgcheck                   = $osgrepo::params::epel_source_gpgcheck,
+  $epel_source_exclude                    = undef,
+  $epel_source_includepkgs                = undef,
+  $epel_debuginfo_mirrorlist              = $osgrepo::params::epel_debuginfo_mirrorlist,
+  $epel_debuginfo_baseurl                 = $osgrepo::params::epel_debuginfo_baseurl,
+  $epel_debuginfo_failovermethod          = $osgrepo::params::epel_debuginfo_failovermethod,
+  $epel_debuginfo_proxy                   = $osgrepo::params::epel_debuginfo_proxy,
+  $epel_debuginfo_enabled                 = $osgrepo::params::epel_debuginfo_enabled,
+  $epel_debuginfo_gpgcheck                = $osgrepo::params::epel_debuginfo_gpgcheck,
+  $epel_debuginfo_exclude                 = undef,
+  $epel_debuginfo_includepkgs             = undef,
+  $epel_testing_source_baseurl            = $osgrepo::params::epel_testing_source_baseurl,
+  $epel_testing_source_failovermethod     = $osgrepo::params::epel_testing_source_failovermethod,
+  $epel_testing_source_proxy              = $osgrepo::params::epel_testing_source_proxy,
+  $epel_testing_source_enabled            = $osgrepo::params::epel_testing_source_enabled,
+  $epel_testing_source_gpgcheck           = $osgrepo::params::epel_testing_source_gpgcheck,
+  $epel_testing_source_exclude            = undef,
+  $epel_testing_source_includepkgs        = undef,
+  $epel_testing_debuginfo_baseurl         = $osgrepo::params::epel_testing_debuginfo_baseurl,
+  $epel_testing_debuginfo_failovermethod  = $osgrepo::params::epel_testing_debuginfo_failovermethod,
+  $epel_testing_debuginfo_proxy           = $osgrepo::params::epel_testing_debuginfo_proxy,
+  $epel_testing_debuginfo_enabled         = $osgrepo::params::epel_testing_debuginfo_enabled,
+  $epel_testing_debuginfo_gpgcheck        = $osgrepo::params::epel_testing_debuginfo_gpgcheck,
+  $epel_testing_debuginfo_exclude         = undef,
+  $epel_testing_debuginfo_includepkgs     = undef,
+  $os_maj_release                         = $osgrepo::params::os_maj_release,
+  $osg_version                            = $osgrepo::params::osg_version,
     $osgrepo_baseurl            = $osgrepo::params::osgrepo_baseurl,
     $osgrepo_enabled            = $osgrepo::params::osgrepo_enabled,
     $osgrepo_failovermethod     = $osgrepo::params::osgrepo_failovermethod,
@@ -29,7 +77,110 @@ class osgrepo (
 
     if $::osfamily == 'RedHat' and $::operatingsystem !~ /Fedora|Amazon/ {
 
-        yumrepo { 'osg':
+    file { "/etc/yum.conf":
+      ensure => present,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0644',
+      source => "puppet:///modules/epel/yum.conf",
+    }
+
+    file { "/etc/rpm/macros.ghc-srpm":
+      ensure => present,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0644',
+      source => "puppet:///modules/epel/macros.hgc-srpm",
+    }
+
+    yumrepo { 'epel-testing':
+      baseurl        => $epel_testing_baseurl,
+      failovermethod => $epel_testing_failovermethod,
+      proxy          => $epel_testing_proxy,
+      enabled        => $epel_testing_enabled,
+      gpgcheck       => $epel_testing_gpgcheck,
+      gpgkey         => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-${os_maj_release}",
+      descr          => "Extra Packages for Enterprise Linux ${os_maj_release} - Testing - \$basearch ",
+      exclude        => $epel_testing_exclude,
+      includepkgs    => $epel_testing_includepkgs,
+    }
+
+    yumrepo { 'epel-testing-debuginfo':
+      baseurl        => $epel_testing_debuginfo_baseurl,
+      failovermethod => $epel_testing_debuginfo_failovermethod,
+      proxy          => $epel_testing_debuginfo_proxy,
+      enabled        => $epel_testing_debuginfo_enabled,
+      gpgcheck       => $epel_testing_debuginfo_gpgcheck,
+      gpgkey         => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-${os_maj_release}",
+      descr          => "Extra Packages for Enterprise Linux ${os_maj_release} - Testing - \$basearch - Debug",
+      exclude        => $epel_testing_debuginfo_exclude,
+      includepkgs    => $epel_testing_debuginfo_includepkgs,
+    }
+
+  yumrepo { 'epel-testing-source':
+    baseurl        => $epel_testing_source_baseurl,
+    failovermethod => $epel_testing_source_failovermethod,
+    proxy          => $epel_testing_source_proxy,
+    enabled        => $epel_testing_source_enabled,
+    gpgcheck       => $epel_testing_source_gpgcheck,
+    gpgkey         => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-${os_maj_release}",
+    descr          => "Extra Packages for Enterprise Linux ${os_maj_release} - Testing - \$basearch - Source",
+    exclude        => $epel_testing_source_exclude,
+    includepkgs    => $epel_testing_source_includepkgs,
+  }
+
+  yumrepo { 'epel':
+    mirrorlist     => $epel_mirrorlist,
+    baseurl        => $epel_baseurl,
+    failovermethod => $epel_failovermethod,
+    proxy          => $epel_proxy,
+    enabled        => $epel_enabled,
+    gpgcheck       => $epel_gpgcheck,
+    gpgkey         => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-${os_maj_release}",
+    descr          => "Extra Packages for Enterprise Linux ${os_maj_release} - \$basearch",
+    exclude        => $epel_exclude,
+    includepkgs    => $epel_includepkgs,
+    }
+
+    yumrepo { 'epel-debuginfo':
+      mirrorlist     => $epel_debuginfo_mirrorlist,
+      baseurl        => $epel_debuginfo_baseurl,
+      failovermethod => $epel_debuginfo_failovermethod,
+      proxy          => $epel_debuginfo_proxy,
+      enabled        => $epel_debuginfo_enabled,
+      gpgcheck       => $epel_debuginfo_gpgcheck,
+      gpgkey         => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-${os_maj_release}",
+      descr          => "Extra Packages for Enterprise Linux ${os_maj_release} - \$basearch - Debug",
+      exclude        => $epel_debuginfo_exclude,
+      includepkgs    => $epel_debuginfo_includepkgs,
+    }
+
+    yumrepo { 'epel-source':
+      mirrorlist     => $epel_source_mirrorlist,
+      baseurl        => $epel_source_baseurl,
+      failovermethod => $epel_source_failovermethod,
+      proxy          => $epel_source_proxy,
+      enabled        => $epel_source_enabled,
+      gpgcheck       => $epel_source_gpgcheck,
+      gpgkey         => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-${os_maj_release}",
+      descr          => "Extra Packages for Enterprise Linux ${os_maj_release} - \$basearch - Source",
+      exclude        => $epel_source_exclude,
+      includepkgs    => $epel_source_includepkgs,
+    }
+
+    file { "/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-${os_maj_release}":
+      ensure => present,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0644',
+      source => "puppet:///modules/epel/RPM-GPG-KEY-EPEL-${os_maj_release}",
+    }
+
+    osgrepo::rpm_gpg_key{ "EPEL-${os_maj_release}":
+      path   => "/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-${os_maj_release}",
+      before => Yumrepo['epel','epel-source','epel-debuginfo','epel-testing','epel-testing-source','epel-testing-debuginfo'],
+    }
+      yumrepo { 'osg':
             descr           => "OSG Software for Enterprise Linux ${::os_maj_version} - \$basearch",
             baseurl         => $osgrepo_baseurl,
             enabled         => $osgrepo_enabled,
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,24 +1,82 @@
 class osgrepo::params {
+  # Setting to 'absent' will fall back to the yum.conf
+  # Setting proxy here will be the default for all repos.
+  #
+  #  If you wish to set a proxy for an individual set of repos,
+  #   you can declare $proxy in that class, and should scope to
+  #   the most specific declaration of proxy.
+  $proxy = 'absent'
 
-    $osgrepo_baseurl = "http://repo.grid.iu.edu/osg/3.2/el${::os_maj_version}/release/\$basearch"
-    $osgrepo_enabled = '1'
-    $osgrepo_failovermethod = 'priority'
-    $osgrepo_gpgcheck = 1
-    $osgrepo_mirrorlist = "http://repo.grid.iu.edu/mirror/osg/3.2/el${::os_maj_version}/release/\$basearch"
-    $osgrepo_priority = '98'
+  if $::operatingsystemmajrelease {
+    $os_maj_release = $::operatingsystemmajrelease
+    } else {
+    $os_versions    = split($::operatingsystemrelease, '[.]')
+    $os_maj_release = $os_versions[0]
+  }
 
-    $osgrepo_testing_baseurl = "http://repo.grid.iu.edu/osg/3.2/el${::os_maj_version}/testing/\$basearch"
-    $osgrepo_testing_enabled = '0'
-    $osgrepo_testing_failovermethod = 'priority'
-    $osgrepo_testing_gpgcheck = '1'
-    $osgrepo_testing_mirrorlist = "http://repo.grid.iu.edu/mirror/osg/3.2/el${::os_maj_version}/testing/\$basearch"
-    $osgrepo_testing_priority = '98'
+  $epel_mirrorlist                        = "https://mirrors.fedoraproject.org/metalink?repo=epel-${os_maj_release}&arch=\$basearch"
+  $epel_baseurl                           = 'absent'
+  $epel_failovermethod                    = 'priority'
+  $epel_proxy                             = $proxy
+  $epel_enabled                           = '1'
+  $epel_gpgcheck                          = '1'
 
-    $osgrepo_development_baseurl = "http://repo.grid.iu.edu/osg/3.2/el${::os_maj_version}/development/\$basearch"
-    $osgrepo_development_enabled = '0'
-    $osgrepo_development_failovermethod = 'priority'
-    $osgrepo_development_gpgcheck = '1'
-    $osgrepo_development_mirrorlist = "http://repo.grid.iu.edu/mirror/osg/3.2/el${::os_maj_version}/development/\$basearch"
-    $osgrepo_development_priority = '98'
+  $epel_debuginfo_mirrorlist              = "https://mirrors.fedoraproject.org/metalink?repo=epel-debug-${os_maj_release}&arch=\$basearch"
+  $epel_debuginfo_baseurl                 = 'absent'
+  $epel_debuginfo_failovermethod          = 'priority'
+  $epel_debuginfo_proxy                   = $proxy
+  $epel_debuginfo_enabled                 = '0'
+  $epel_debuginfo_gpgcheck                = '1'
+
+  $epel_source_mirrorlist                 = "https://mirrors.fedoraproject.org/metalink?repo=epel-source-${os_maj_release}&arch=\$basearch"
+  $epel_source_baseurl                    = 'absent'
+  $epel_source_failovermethod             = 'priority'
+  $epel_source_proxy                      = $proxy
+  $epel_source_enabled                    = '0'
+  $epel_source_gpgcheck                   = '1'
+
+  #$epel_testing_baseurl                   = "https://download.fedoraproject.org/pub/epel/testing/${os_maj_release}/\$basearch"
+  $epel_testing_mirrorlist                = "https://mirrors.fedoraproject.org/metalink?repo=testing-epel${os_maj_release}&arch=\$basearch"
+  $epel_testing_failovermethod            = 'priority'
+  $epel_testing_proxy                     = $proxy
+  $epel_testing_enabled                   = '0'
+  $epel_testing_gpgcheck                  = '1'
+
+  #$epel_testing_debuginfo_baseurl         = "https://download.fedoraproject.org/pub/epel/testing/${os_maj_release}/\$basearch/debug"
+  $epel_testing_debuginfo_mirrorlist      = "https://mirrors.fedoraproject.org/metalink?repo=testing-debug-epel${os_maj_release}&arch=\$basearch"
+  $epel_testing_debuginfo_failovermethod  = 'priority'
+  $epel_testing_debuginfo_proxy           = $proxy
+  $epel_testing_debuginfo_enabled         = '0'
+  $epel_testing_debuginfo_gpgcheck        = '1'
+
+  #$epel_testing_source_baseurl            = "https://download.fedoraproject.org/pub/epel/testing/${os_maj_release}/SRPMS"
+  $epel_testing_source_mirrorlist      = "https://mirrors.fedoraproject.org/metalink?repo=testing-source-epel${os_maj_release}&arch=\$basearch"
+  $epel_testing_source_failovermethod     = 'priority'
+  $epel_testing_source_proxy              = $proxy
+  $epel_testing_source_enabled            = '0'
+  $epel_testing_source_gpgcheck           = '1'
+
+  $osg_version = 3.3
+
+  #$osgrepo_baseurl = "http://repo.grid.iu.edu/osg/$osg_version/el${::os_maj_version}/release/\$basearch"
+  $osgrepo_enabled = '1'
+  $osgrepo_failovermethod = 'priority'
+  $osgrepo_gpgcheck = 1
+  $osgrepo_mirrorlist = "http://repo.grid.iu.edu/mirror/osg/$osg_version/el${::os_maj_version}/release/\$basearch"
+  $osgrepo_priority = '98'
+
+  #$osgrepo_testing_baseurl = "http://repo.grid.iu.edu/osg/$osg_version/el${::os_maj_version}/testing/\$basearch"
+  $osgrepo_testing_enabled = '0'
+  $osgrepo_testing_failovermethod = 'priority'
+  $osgrepo_testing_gpgcheck = '1'
+  $osgrepo_testing_mirrorlist = "http://repo.grid.iu.edu/mirror/osg/$osg_version/el${::os_maj_version}/testing/\$basearch"
+  $osgrepo_testing_priority = '98'
+
+  #$osgrepo_development_baseurl = "http://repo.grid.iu.edu/osg/$osg_version/el${::os_maj_version}/development/\$basearch"
+  $osgrepo_development_enabled = '0'
+  $osgrepo_development_failovermethod = 'priority'
+  $osgrepo_development_gpgcheck = '1'
+  $osgrepo_development_mirrorlist = "http://repo.grid.iu.edu/mirror/osg/$osg_version/el${::os_maj_version}/development/\$basearch"
+  $osgrepo_development_priority = '98'
 
 }
