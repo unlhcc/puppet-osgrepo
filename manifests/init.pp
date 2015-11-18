299a39b7eb6fc748d51c3ff8e3f3fbcd288a75ef
Garhan Attebury <garhan.attebury@unl.edu>
2015-11-18 13:20:40 -0600

    added osg-contrib and cleaned up some lint

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,25 +3,33 @@
 #
 # Sets up OSG Software for Enterprise Linux repos
 #
-
 class osgrepo (
-  $os_maj_release                         = $osgrepo::params::os_maj_release,
-  $osg_version                            = $osgrepo::params::osg_version,
-  #  $osgrepo_baseurl            = $osgrepo::params::osgrepo_baseurl,
+
+    $os_maj_release             = $osgrepo::params::os_maj_release,
+    $osg_version                = $osgrepo::params::osg_version,
+
+    #$osgrepo_baseurl            = $osgrepo::params::osgrepo_baseurl,
     $osgrepo_enabled            = $osgrepo::params::osgrepo_enabled,
     $osgrepo_failovermethod     = $osgrepo::params::osgrepo_failovermethod,
     $osgrepo_gpgcheck           = $osgrepo::params::osgrepo_gpgcheck,
     $osgrepo_mirrorlist         = $osgrepo::params::osgrepo_mirrorlist,
     $osgrepo_priority           = $osgrepo::params::osgrepo_priority,
 
-  #  $osgrepo_testing_baseurl        = $osgrepo::params::osgrepo_testing_baseurl,
+    #$osgrepo_contrib_baseurl        = $osgrepo::params::osgrepo_contrib_baseurl,
+    $osgrepo_contrib_enabled        = $osgrepo::params::osgrepo_contrib_enabled,
+    $osgrepo_contrib_failovermethod = $osgrepo::params::osgrepo_contrib_failovermethod,
+    $osgrepo_contrib_gpgcheck       = $osgrepo::params::osgrepo_contrib_gpgcheck,
+    $osgrepo_contrib_mirrorlist     = $osgrepo::params::osgrepo_contrib_mirrorlist,
+    $osgrepo_contrib_priority       = $osgrepo::params::osgrepo_contrib_priority,
+
+    #$osgrepo_testing_baseurl        = $osgrepo::params::osgrepo_testing_baseurl,
     $osgrepo_testing_enabled        = $osgrepo::params::osgrepo_testing_enabled,
     $osgrepo_testing_failovermethod = $osgrepo::params::osgrepo_testing_failovermethod,
     $osgrepo_testing_gpgcheck       = $osgrepo::params::osgrepo_testing_gpgcheck,
     $osgrepo_testing_mirrorlist     = $osgrepo::params::osgrepo_testing_mirrorlist,
     $osgrepo_testing_priority       = $osgrepo::params::osgrepo_testing_priority,
 
-  #  $osgrepo_development_baseurl        = $osgrepo::params::osgrepo_development_baseurl,
+    #$osgrepo_development_baseurl        = $osgrepo::params::osgrepo_development_baseurl,
     $osgrepo_development_enabled        = $osgrepo::params::osgrepo_development_enabled,
     $osgrepo_development_failovermethod = $osgrepo::params::osgrepo_development_failovermethod,
     $osgrepo_development_gpgcheck       = $osgrepo::params::osgrepo_development_gpgcheck,
@@ -32,46 +40,56 @@ class osgrepo (
     if $::osfamily == 'RedHat' and $::operatingsystem !~ /Fedora|Amazon/ {
 
       yumrepo { 'osg':
-            descr           => "OSG Software for Enterprise Linux ${::os_maj_version} - \$basearch",
-            baseurl         => $osgrepo_baseurl,
-            enabled         => $osgrepo_enabled,
-            failovermethod  => $osgrepo_failovermethod,
-            gpgcheck        => $osgrepo_gpgcheck,
-            mirrorlist      => $osgrepo_mirrorlist,
-            priority        => $osgrepo_priority,
+            descr          => "OSG Software for Enterprise Linux ${::os_maj_version} - \$basearch",
+            baseurl        => $osgrepo_baseurl,
+            enabled        => $osgrepo_enabled,
+            failovermethod => $osgrepo_failovermethod,
+            gpgcheck       => $osgrepo_gpgcheck,
+            mirrorlist     => $osgrepo_mirrorlist,
+            priority       => $osgrepo_priority,
+        }
+
+        yumrepo { 'osg-contrib':
+            descr          => "OSG Software for Enterprise Linux ${::os_maj_version} - Contributed - \$basearch",
+            baseurl        => $osgrepo_contrib_baseurl,
+            enabled        => $osgrepo_contrib_enabled,
+            failovermethod => $osgrepo_contrib_failovermethod,
+            gpgcheck       => $osgrepo_contrib_gpgcheck,
+            mirrorlist     => $osgrepo_contrib_mirrorlist,
+            priority       => $osgrepo_contrib_priority,
         }
 
         yumrepo { 'osg-testing':
-            descr           => "OSG Software for Enterprise Linux ${::os_maj_version} - Testing - \$basearch",
-            baseurl         => $osgrepo_testing_baseurl,
-            enabled         => $osgrepo_testing_enabled,
-            failovermethod  => $osgrepo_testing_failovermethod,
-            gpgcheck        => $osgrepo_testing_gpgcheck,
-            mirrorlist      => $osgrepo_testing_mirrorlist,
-            priority        => $osgrepo_testing_priority,
+            descr          => "OSG Software for Enterprise Linux ${::os_maj_version} - Testing - \$basearch",
+            baseurl        => $osgrepo_testing_baseurl,
+            enabled        => $osgrepo_testing_enabled,
+            failovermethod => $osgrepo_testing_failovermethod,
+            gpgcheck       => $osgrepo_testing_gpgcheck,
+            mirrorlist     => $osgrepo_testing_mirrorlist,
+            priority       => $osgrepo_testing_priority,
         }
 
         yumrepo { 'osg-development':
-            descr           => "OSG Software for Enterprise Linux ${::os_maj_version} - Development - \$basearch",
-            baseurl         => $osgrepo_development_baseurl,
-            enabled         => $osgrepo_development_enabled,
-            failovermethod  => $osgrepo_development_failovermethod,
-            gpgcheck        => $osgrepo_development_gpgcheck,
-            mirrorlist      => $osgrepo_development_mirrorlist,
-            priority        => $osgrepo_development_priority,
+            descr          => "OSG Software for Enterprise Linux ${::os_maj_version} - Development - \$basearch",
+            baseurl        => $osgrepo_development_baseurl,
+            enabled        => $osgrepo_development_enabled,
+            failovermethod => $osgrepo_development_failovermethod,
+            gpgcheck       => $osgrepo_development_gpgcheck,
+            mirrorlist     => $osgrepo_development_mirrorlist,
+            priority       => $osgrepo_development_priority,
         }
 
         file { '/etc/pki/rpm-gpg/RPM-GPG-KEY-OSG':
-            ensure  => present,
-            owner   => 'root',
-            group   => 'root',
-            mode    => '0644',
-            source  => 'puppet:///modules/osgrepo/RPM-GPG-KEY-OSG',
+            ensure => present,
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0644',
+            source => 'puppet:///modules/osgrepo/RPM-GPG-KEY-OSG',
         }
 
         osgrepo::rpm_gpg_key { 'OSG':
-            path    => '/etc/pki/rpm-gpg/RPM-GPG-KEY-OSG',
-            before  => Yumrepo[ 'osg', 'osg-testing', 'osg-development' ],
+            path   => '/etc/pki/rpm-gpg/RPM-GPG-KEY-OSG',
+            before => Yumrepo[ 'osg', 'osg-testing', 'osg-development' ],
         }
 
     } else {
