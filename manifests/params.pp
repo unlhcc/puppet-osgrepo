9709204e2f4ca7951b2cb870a41783299c37fb55
Garhan Attebury <garhan.attebury@unl.edu>
2015-11-18 13:29:53 -0600

    I don't always use the wrong scope, but when I do, it's top scope

--- manifests/params.pp
+++ manifests/params.pp
@@ -9,32 +9,32 @@ class osgrepo::params {
 
   $osg_version = 3.3
 
-  #$osgrepo_baseurl = "http://repo.grid.iu.edu/osg/${::osg_version}/el${::os_maj_version}/release/\$basearch"
+  #$osgrepo_baseurl = "http://repo.grid.iu.edu/osg/${osg_version}/el${::os_maj_version}/release/\$basearch"
   $osgrepo_enabled = '1'
   $osgrepo_failovermethod = 'priority'
   $osgrepo_gpgcheck = 1
-  $osgrepo_mirrorlist = "http://repo.grid.iu.edu/mirror/osg/${::osg_version}/el${::os_maj_version}/release/\$basearch"
+  $osgrepo_mirrorlist = "http://repo.grid.iu.edu/mirror/osg/${osg_version}/el${::os_maj_version}/release/\$basearch"
   $osgrepo_priority = '98'
 
-  #$osgrepo_contrib_baseurl = "http://repo.grid.iu.edu/osg/${::osg_version}/el${::os_maj_version}/contrib/\$basearch"
+  #$osgrepo_contrib_baseurl = "http://repo.grid.iu.edu/osg/${osg_version}/el${::os_maj_version}/contrib/\$basearch"
   $osgrepo_contrib_enabled = '1'
   $osgrepo_contrib_failovermethod = 'priority'
   $osgrepo_contrib_gpgcheck = '1'
-  $osgrepo_contrib_mirrorlist = "http://repo.grid.iu.edu/mirror/osg/${::osg_version}/el${::os_maj_version}/contrib/\$basearch"
+  $osgrepo_contrib_mirrorlist = "http://repo.grid.iu.edu/mirror/osg/${osg_version}/el${::os_maj_version}/contrib/\$basearch"
   $osgrepo_contrib_priority = '98'
 
-  #$osgrepo_testing_baseurl = "http://repo.grid.iu.edu/osg/${::osg_version}/el${::os_maj_version}/testing/\$basearch"
+  #$osgrepo_testing_baseurl = "http://repo.grid.iu.edu/osg/${osg_version}/el${os_maj_version}/testing/\$basearch"
   $osgrepo_testing_enabled = '0'
   $osgrepo_testing_failovermethod = 'priority'
   $osgrepo_testing_gpgcheck = '1'
-  $osgrepo_testing_mirrorlist = "http://repo.grid.iu.edu/mirror/osg/${::osg_version}/el${::os_maj_version}/testing/\$basearch"
+  $osgrepo_testing_mirrorlist = "http://repo.grid.iu.edu/mirror/osg/${osg_version}/el${::os_maj_version}/testing/\$basearch"
   $osgrepo_testing_priority = '98'
 
-  #$osgrepo_development_baseurl = "http://repo.grid.iu.edu/osg/${::osg_version}/el${::os_maj_version}/development/\$basearch"
+  #$osgrepo_development_baseurl = "http://repo.grid.iu.edu/osg/${osg_version}/el${::os_maj_version}/development/\$basearch"
   $osgrepo_development_enabled = '0'
   $osgrepo_development_failovermethod = 'priority'
   $osgrepo_development_gpgcheck = '1'
-  $osgrepo_development_mirrorlist = "http://repo.grid.iu.edu/mirror/osg/${::osg_version}/el${::os_maj_version}/development/\$basearch"
+  $osgrepo_development_mirrorlist = "http://repo.grid.iu.edu/mirror/osg/${osg_version}/el${::os_maj_version}/development/\$basearch"
   $osgrepo_development_priority = '98'
 
 }
